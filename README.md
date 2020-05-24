970e45523a4b82660e543929b9cb430909644a29
Levi Cole <levi@skape.co>
2020-05-24 22:27:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Basic Usage
+Include the plugin in your `webpack.mix.js` file as follows:
+```javascript
+const mix = require('laravel-mix');
+const MinifyLaravelJigsawOutputPlugin = require( 'minify-laravel-jigsaw-output-plugin' );
+require('laravel-mix-jigsaw');
+
+mix.disableSuccessNotifications();
+mix.setPublicPath('source/assets/build');
+
+mix.jigsaw()
+    .js('source/_assets/js/main.js', 'js')
+    .sass('source/_assets/sass/main.scss', 'css')
+    .options({
+        processCssUrls: false,
+    })
+    .version();
+
+mix.webpackConfig({
+    plugins: [
+        new MinifyLaravelJigsawOutputPlugin({
+	        verbose: true
+        })
+    ]
+});
+```
+
+# Options
+The below options can be passed to the plugin.
+
+|Option|Description|Default|
+|--|--|--|
+| `rules` | This is an object of rules to be used by [HTMLMinifier](https://kangax.github.io/html-minifier/). | `{ collapseWhitespace: true }` |
+| `env` | Set a specific build environment for dynamically guessing the out put directories. By default uses the parameter sent to webpack e.g. `npm run production` | `local` |
+| `verbose` | Wether or not to print logs to the console. | `false` |
+| `test` | The regular expression used before modifying a file | `/\.html$/` |
+| `encoding` | The file encoding used to read the output. | `utf8` |
---
+++ index.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const fs = require( 'fs' );
+const path = require( 'path' );
+const argv = require( 'yargs' ).argv;
+const minifier = require( 'html-minifier' ).minify;
+
+class MinifyLaravelJigsawOutputPlugin {
+
+    constructor( options = {} ) {
+        this.options  = options;
+        this.env      = this.options.env || argv.env || 'local';
+        this.inPath   = this.options.input || this.getPath( `build_${this.env}` );
+        this.outPath  = this.options.output || this.inPath;
+        this.pattern  = this.options.test || /\.html$/;
+        this.encoding = this.options.encoding || 'utf8';
+        this.rules    = this.options.rules || { collapseWhitespace: true };
+    }
+
+    getPath( string ) {
+        if ( fs.existsSync( `./${string}` ) ) {
+            return path.normalize( `./${string}` );
+        }
+    }
+
+    log( string ) {
+        if ( this.options.verbose ) {
+            console.log( string );
+        }
+    }
+
+    minfifyOutput( inDir, outDir ) {
+
+        outDir = outDir || inDir;
+
+        fs.readdir( inDir, ( error, files ) => {
+            if ( error ) {
+                throw error;
+            }
+
+            files.forEach( file => {
+                let inputFile = path.resolve( inDir, file );
+
+                if ( fs.statSync( inputFile ).isDirectory() ) {
+                    this.minfifyOutput( inputFile, path.resolve( outDir, file ) );
+                } else {
+
+                    let source = fs.readFileSync( inputFile, this.encoding );
+
+                    if ( this.pattern.test( inputFile ) ) {
+                        this.log( `Minifing ${inputFile}` );
+                        let result = minifier( source, this.rules );
+                        let outputFile = path.resolve( outDir, file );
+                        fs.writeFileSync( outputFile, result );
+                    }
+                }
+            } );
+        } );
+    }
+
+    apply( compiler ) {
+        compiler.hooks.jigsawWebpackBuildDone.tap( 'MinifyLaravelJigsawOutputPlugin', () => {
+
+            this.log( 'Starting to minimize output...' );
+
+            if ( !this.path ) {
+                throw new Error( `Build location for "${this.env}" does not exist.` );
+            }
+
+            const inDir = path.resolve( this.inPath );
+            const outDir = path.resolve( this.outPath );
+
+            this.minfifyOutput( inDir, outDir );
+
+        } );
+    }
+}
+
+module.exports = MinifyLaravelJigsawOutputPlugin;
